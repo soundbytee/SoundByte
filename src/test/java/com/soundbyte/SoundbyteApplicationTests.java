98d580ccebc27a5a042bdb23a97d509882de29ba
Samuel Yeboah sy433 <sy433@cornell.edu>
2025-07-07 21:11:10 -0400

    Springboot Setup

---
+++ src/test/java/com/soundbyte/SoundbyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundbyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
